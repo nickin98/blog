1f41dcab0084c2cec9baa9f8b0d1e2fed4e39537
Nikita Zlobin <zlo-nic@mail.ru>
2023-02-27 20:26:15 +0500

    MASTER

---
+++ app/javascript/routes/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "../components/Main";
+
+export default (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  </Router>
+);
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: [
+    './app/views/**/*',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js'
+  ]
+}
