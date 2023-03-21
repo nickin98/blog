c70be2b6574572fbc0aeaa752125c82cc48985f7
Nikita Zlobin <zlo-nic@mail.ru>
2023-03-21 21:36:25 +0500

    other_complete

--- app/javascript/components/Main.jsx
+++ app/javascript/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import TagsList from "./navigation/tags/TagsList";
 import FirstArticle from "./articles/FirstArticle";
-import { OtherArticle } from "./articles/OtherArticle";
+import OtherArticle from "./articles/OtherArticle";
 import SubscribeEmail from "./custom_blocks/SubscribeEmail";
 import Calendar from "./custom_blocks/Calendar";
 import Glossary from "./custom_blocks/Glossary";
@@ -13,29 +13,28 @@ export default () => (
     <div className="mt-12"/>
     <TagsList/>
     <FirstArticle/>
-    {/* <div className="mt-12"/> */}
-    {/* <div className="flex flex-wrap gap-x-1 gap-y-3 justify-between">
+    <div className="flex flex-col space-y-7 mt-8 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-4 sm:mt-6 lg:grid-cols-3 lg:gap-6 xl:mt-12">
       <OtherArticle/>
       <OtherArticle/>
       <OtherArticle/>
-      <SubscribeEmail/>
-      <OtherArticle/>
-      <OtherArticle/>
-      <OtherArticle/>
-      <OtherArticle/>
-      <OtherArticle/>
-      <Calendar/>
-      <Glossary/>
+      {/* <SubscribeEmail/> */}
       <OtherArticle/>
       <OtherArticle/>
       <OtherArticle/>
       <OtherArticle/>
       <OtherArticle/>
+      {/* <Calendar/> */}
+      {/* <Glossary/> */}
       <OtherArticle/>
       <OtherArticle/>
-      <OfferArticle/>
-    </div> */}
-    {/* <div className="mt-12"/> */}
+      <OtherArticle/>
+      <OtherArticle/>
+      <OtherArticle/>
+      <OtherArticle/>
+      <OtherArticle/>
+      {/* <OfferArticle/> */}
+    </div>
+    <div className="mt-12"/>
     {/* <Paginations/> */}
     {/* <div className="mb-12"/> */}
   </>
--- app/javascript/components/articles/OtherArticle.tsx
+++ app/javascript/components/articles/OtherArticle.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Tag from "../navigation/tags/Tag";
 import { Statistic } from "./statistic";
+import classNames from "classnames";
 
 const OtherArticle = () => {
+  const styles = classNames(
+    `flex flex-col relative rounded-3xl shadow-previeArticle hover:shadow-hoverPrevieArticle cursor-pointer transition duration-300`
+  );
   return (
-    <div className="flex flex-col w-[26rem] relative rounded-[22px] shadow-previeArticle hover:shadow-hoverPrevieArticle cursor-pointer transition duration-300">
+    <div className={styles}>
       {
         <Tag
           text="Новое"
@@ -12,16 +16,15 @@ const OtherArticle = () => {
           type="absoluteNews"
         />
       }
-      <img className="w-[38rem] rounded-[23px] h-48" src="cover.png" />
-      <div className="flex flex-col justify-between pt-6 px-4 pb-[22px]">
-        <h2 className="text-lg font-medium">
+      <img className="rounded-3xl" src="cover.png" />
+      <div className="flex flex-col justify-between py-6 px-4">
+        <h2 className="text-lg md:text-base xl:text-lg font-medium mb-4">
           Улучшить результаты другого подрядчика и получить 200+ лидов в кризис? Кейс Титансофт о продвижении застройщика
         </h2>
-        <div className="mt-4"/>
         <Statistic/>
       </div>
     </div>
   )
 }
 
-export {OtherArticle}
+export default OtherArticle
